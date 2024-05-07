605ab721d73f0da47f42a8943f78664ee13faa84
Rais A. <nandaliyan.rais@gmail.com>
2024-05-07 20:49:37 +0700

    add initial project, database, and crud for stocks and comments

---
+++ Repositories/StockRepository.cs
@@ -0,0 +1,78 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using api.Data;
+using api.Dtos.Stock;
+using api.Interfaces;
+using api.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace api.Repositories
+{
+    public class StockRepository : IStockRepository
+    {
+        private readonly ApplicationDBContext _context;
+
+        public StockRepository(ApplicationDBContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<Stock> CreateAsync(Stock stockModel)
+        {
+            await _context.Stocks.AddAsync(stockModel);
+            await _context.SaveChangesAsync();
+            return stockModel;
+        }
+
+        public async Task<Stock?> DeleteAsync(int id)
+        {
+            var stockModel = await _context.Stocks.FirstOrDefaultAsync(x => x.Id == id);
+
+            if (stockModel == null)
+            {
+                return null;
+            }
+
+            _context.Stocks.Remove(stockModel);
+            await _context.SaveChangesAsync();
+            return stockModel;
+        }
+
+        public async Task<List<Stock>> GetAllAsync()
+        {
+            return await _context.Stocks.Include(c => c.Comments).ToListAsync();
+        }
+
+        public async Task<Stock?> GetByIdAsync(int id)
+        {
+            return await _context.Stocks.Include(c => c.Comments).FirstOrDefaultAsync(x => x.Id == id);
+        }
+
+        public Task<bool> StockExists(int id)
+        {
+            return _context.Stocks.AnyAsync(s => s.Id == id);
+        }
+
+        public async Task<Stock?> UpdateAsync(int id, UpdateStockRequest stock)
+        {
+            var existingStock = await _context.Stocks.FirstOrDefaultAsync(x => x.Id == id);
+
+            if (existingStock == null)
+            {
+                return null;
+            }
+
+            existingStock.Symbol = stock.Symbol;
+            existingStock.CompanyName = stock.CompanyName;
+            existingStock.Purchase = stock.Purchase;
+            existingStock.LastDiv = stock.LastDiv;
+            existingStock.Industry = stock.Industry;
+            existingStock.MarketCap = stock.MarketCap;
+
+            await _context.SaveChangesAsync();
+            return existingStock;
+        }
+    }
+}
